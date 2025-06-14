b21692c75d260541cf8894c3b8d4cad1dd73d14b
Rohit Kumar <rohitkhicher001@gmail.com>
2025-06-14 17:46:34 +0530

    added weather api

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: WeatherPage(), theme: ThemeData.dark());
+    return MaterialApp(home: WeatherScreen(), theme: ThemeData.dark());
   }
 }
